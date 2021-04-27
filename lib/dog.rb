60768c00691d7a6491da8145e85aa5004b051f83
Dan Powell <powell956@gmail.com>
2021-04-27 19:17:33 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Add your code here
+class Dog
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def self.all
+        @@all
+    end 
+
+    def self.print_all
+        @@all.map {|dog| puts dog.name }
+    end 
+
+    def save
+        @@all << self
+    end
+
+    def self.clear_all
+        @@all.clear
+    end 
+
+end 
